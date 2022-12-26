b5e307ed0a676a3805f432ab3fa5fe32d6a83d88
tuanlysc <93781654+tuanlysc@users.noreply.github.com>
2022-12-26 17:42:57 +0700

    Test

---
+++ no1.py
@@ -0,0 +1,10 @@
+a=int(input("nhập số a:"))
+b=int(input("nhập số b:"))
+tong=a+b
+hieu=a-b
+tich=a*b
+thuong=a/b
+print("Tổng số",a,"và",b,"là:",tong)
+print("Hiệu số",a,"và",b,"là:",hieu)
+print("Tích số",a,"và",b,"là:",tich)
+print("Thương số",a,"và",b,"là:",thuong)
